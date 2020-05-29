bb639e3d0dc8d1d62b8fc4813f3fc7504ba530a6
EvilBengt <anton.jh@hotmail.com>
2020-05-29 20:57:50 +0200

    DI config cleanup

--- config/di/forumDatabase.php
+++ config/di/forumDatabase.php
@@ -8,7 +8,52 @@ return [
         "userManager" => [
             "shared" => true,
             "callback" => function () {
-                return new EVB\Forum\Database\UserManager($this->get("db"));
+                return new EVB\Forum\Database\UserManager(
+                    $this->get("db"),
+                    $this->get("textfilter")
+                );
+            }
+        ],
+        "questionManager" => [
+            "shared" => true,
+            "callback" => function () {
+                return new EVB\Forum\Database\QuestionManager(
+                    $this->get("db"),
+                    $this->get("userManager"),
+                    $this->get("commentManager"),
+                    $this->get("answerManager"),
+                    $this->get("textfilter"),
+                    $this->get("tagManager")
+                );
+            }
+        ],
+        "tagManager" => [
+            "shared" => true,
+            "callback" => function () {
+                return new EVB\Forum\Database\TagManager(
+                    $this->get("db")
+                );
+            }
+        ],
+        "answerManager" => [
+            "shared" => true,
+            "callback" => function () {
+                return new EVB\Forum\Database\AnswerManager(
+                    $this->get("db"),
+                    $this->get("userManager"),
+                    $this->get("commentManager"),
+                    $this->get("textfilter")
+                );
+            }
+        ],
+        "commentManager" => [
+            "shared" => true,
+            "callback" => function () {
+                return new EVB\Forum\Database\CommentManager(
+                    $this->get("db"),
+                    $this->get("userManager"),
+                    $this->get("textfilter")
+                );
             }
         ],
     ],
